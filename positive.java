7f951d91dbea8e23d56d7207d1f8d7fafbc5b70f
DeepakAarthi <aarthiselvamani3@gmail.com>
2017-06-16 15:28:04 +0530

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,14 @@
+package postive;
+public class positive{
+public static void main(String[] args){
+int input;
+Scanner s=new Scanner(System.in);
+input=s.nextInt();
+if(input<=0)
+{
+System.out.println("psitive no:"+input);
+}
+else{
+System.out.println("negative no:+input);
+}
+}}
