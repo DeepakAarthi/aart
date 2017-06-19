c9dc9131575ea4ca2abd44031acb2743d9d3a054
DeepakAarthi <aarthiselvamani3@gmail.com>
2017-06-19 17:43:53 +0530

    Add files via upload

---
+++ Fact.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Fact {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int no, fect=1;
+		Scanner s=new Scanner(System.in);
+	    System.out.println("Enter any number :");
+		no=s.nextInt();
+	    for(int i=1; i<=no; i++)
+		{
+		fect=fect*i;
+		}
+		System.out.println("Factorial is :" +fect);
+		}
+	
+	}
+
+
