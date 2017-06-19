40c04fc7f71d5690728a58b622039ad1fb660fe2
DeepakAarthi <aarthiselvamani3@gmail.com>
2017-06-19 17:16:07 +0530

    Create alpha.java

---
+++ alpha.java
@@ -0,0 +1,10 @@
+public class alpha{
+public static void main(String[] args){
+char z='a';
+if(z>=a&&z<=z||z>=A&&z<=Z){
+System.out.println("alphabets");
+}
+else{
+System.out.println("special characters');
+}
+}}
