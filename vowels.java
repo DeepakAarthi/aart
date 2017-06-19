ba445cd9cd97ee07087ef7f2382455f30435ba87
DeepakAarthi <aarthiselvamani3@gmail.com>
2017-06-19 15:17:58 +0530

    Create vowels.java

---
+++ vowels.java
@@ -0,0 +1,9 @@
+public class vowels{
+public static void main(String[] args){
+char a='i';
+if(a==a||a==e||a==i||a==o||a==u){
+System.out.println("vowels");}
+else{
+System.out.println("constants");}
+}
+}
