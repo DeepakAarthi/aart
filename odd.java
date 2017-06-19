cb105c6b3cce55e10ed2b4484ad5cc1f3497b18e
DeepakAarthi <aarthiselvamani3@gmail.com>
2017-06-19 17:53:00 +0530

    Update and rename even.java to odd.java

---
+++ odd.java
@@ -0,0 +1,16 @@
+public class even{
+public static void main (String[] args){
+int a=10;
+int b=20;
+int c=0;
+  int count=0;
+  if(a%2>=0&&b%2<=0){
+  count++
+System.out.println("even no"+count);
+}
+else{
+  c++
+System.out.println("odd no"+c);
+}
+}
+}
