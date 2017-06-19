eb200aff464cd0652082d8021b0851b83ed2e05e
DeepakAarthi <aarthiselvamani3@gmail.com>
2017-06-19 14:41:28 +0530

    Update leapyear.java

--- leapyear.java
+++ leapyear.java
@@ -10,7 +10,7 @@ public class leapyear {
 	        Scanner as=new Scanner(System.in);
 	        System.out.println("Enter the year");
 	        arg=as.nextInt();
-	       	        if(arg%4==0||arg%400==0){
+	       	        if(arg%4==0){
 	        	 System.out.println("Leap year");
 	        }else{
 	        	 System.out.println("Not a leap year");
