9c7c39f824f659d95bda310000c3535145151ef5
DeepakAarthi <aarthiselvamani3@gmail.com>
2017-06-19 15:12:59 +0530

    Update hello.java

--- hello.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-void main(){
-    int count;
-  for( count=1;count<=5;count++){
-    printf("\n Hello");}
-    return 0;
-}
---
+++ hello.java
@@ -0,0 +1,7 @@
+public class hello{
+    public static void main(String[] args){
+        int count;
+  for( count=1;count<=5;count++){
+   System.out.println("\n Hello");}
+    return 0;
+}
