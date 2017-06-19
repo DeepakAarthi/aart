743d77133caf65f4cdbb6aa307b5dd6e950fb231
DeepakAarthi <aarthiselvamani3@gmail.com>
2017-06-19 17:40:31 +0530

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,26 @@
+
+public class Armstrong {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int c=0,a,temp;  
+		    int n=153; 
+		    temp=n;  
+		    while(n>0)  
+		    {  
+		    a=n%10;  
+		    n=n/10;  
+		    c=c+(a*a*a);  
+		    }  
+		    if(temp==c)  
+		    System.out.println("armstrong number");   
+		    else  
+		        System.out.println("Not armstrong number");   
+		   }  
+		
+	}
+
+
