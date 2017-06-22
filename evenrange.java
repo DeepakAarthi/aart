048117131a73185fb30095c393af92ea57e2c2ca
DeepakAarthi <aarthiselvamani3@gmail.com>
2017-06-22 10:48:23 +0530

    Add files via upload

---
+++ evenrange.java
@@ -0,0 +1,15 @@
+package evenrange;
+
+public class evenrange {
+
+	public static void main(String[] args) {
+		int a,i;
+		for(i=23;i<=57;i++){
+			if(i%2==0){
+				System.out.print("\t"+i);
+			}
+		}
+
+	}
+
+}
