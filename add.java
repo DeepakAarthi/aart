36db199dec1788dce7b030c231989b2be0081719
DeepakAarthi <aarthiselvamani3@gmail.com>
2017-06-25 10:41:36 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,10 @@
+public class add{
+public static void main(String[] args){
+int n=10;s=45;
+for(int i=0;i<=n;i++){
+int sum=sum+i;}
+System.out.println(sum);
+for(int j=15;i<=45;i++){
+if(j%2!=0){
+int odd=odd+j;}}
+System.out.println(odd);
