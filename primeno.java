4d1b1acaad1c25133a2128bc72e08b478c27e1f1
DeepakAarthi <aarthiselvamani3@gmail.com>
2017-06-25 09:58:36 +0530

    Add files via upload

---
+++ primeno.java
@@ -0,0 +1,16 @@
+
+public class primeno {
+
+	public static void main(String[] args) {
+		int i,count=0;
+		for(int n=10;n<=99;n++){
+		for(i=2;i<=n;i++){
+			if(n%i==0){
+				System.out.println(""+n);
+			}
+		}}
+
+
+	}
+
+}
