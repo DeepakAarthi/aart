e2c325ed18b296ca1a641968487df8e83825e665
DeepakAarthi <aarthiselvamani3@gmail.com>
2017-06-25 10:25:23 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,14 @@
+package reverse;
+
+public class reverse {
+
+	public static void main(String[] args) {
+	String  a="1345";
+	String reverse=new StringBuffer(a).reverse().to String();
+System.out.println(""+a);
+System.out.println(""+reverse);
+	
+
+	}
+
+}
