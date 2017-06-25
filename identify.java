aad5a1ceb5c0dbea9b9d07eaecf8e2d57ab8d385
DeepakAarthi <aarthiselvamani3@gmail.com>
2017-06-25 10:28:11 +0530

    Add files via upload

---
+++ identify.java
@@ -0,0 +1,19 @@
+package identify;
+
+public class identify {
+
+	public static void main(String[] args) {
+		 String strUpper, strLower;
+		 Scanner scan = new Scanner(System.in);
+	        
+	        System.out.print("Enter one word/name in Uppercase : ");
+	        strUpper = scan.nextLine();
+	        
+	        strLower = strUpper.toLowerCase();
+	        System.out.print("Equivalent Word/Name in Lowercase : " + strLower);
+	   
+		
+
+	}
+
+}
