dbfa07eb93afcd5ab529bec736ae6111bc9cb7fc
DeepakAarthi <aarthiselvamani3@gmail.com>
2017-07-03 17:05:24 +0530

    Update and rename rev.java to prev.java

--- prev.java
+++ prev.java
@@ -3,7 +3,7 @@ package reverse;
 public class reverse {
 
 	public static void main(String[] args) {
-	String  a="wipro";
+	String  a="1234";
 	String reverse=new StringBuffer(a).reverse().to String();
 System.out.println(""+a);
 System.out.println(""+reverse);
