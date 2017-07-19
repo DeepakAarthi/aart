67f2546917be6700d0d75f90db750d7ff4f017e4
DeepakAarthi <aarthiselvamani3@gmail.com>
2017-07-19 14:21:29 +0530

    Create nonrepeat.java

---
+++ nonrepeat.java
@@ -0,0 +1,18 @@
+
+
+public class nonrepeat {
+public static void main(String[] args){
+	int a[]={1,2,3,2,1};
+	int x[]=new int[a.length];
+	
+	for(int i=0;i<a.length;i++){
+		int b=a[i];
+	x[b]++;
+		}
+	for( int j=0;j<x.length;j++)
+if(x[j]==1){
+	System.out.println(j);
+}
+	}
+}
+
