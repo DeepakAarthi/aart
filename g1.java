726d737328c2dbf64e083ef00a1145c6c23868ba
DeepakAarthi <aarthiselvamani3@gmail.com>
2017-07-19 12:10:11 +0530

    Add files via upload

---
+++ g1.java
@@ -0,0 +1,14 @@
+package guvi;
+
+import java.util.Arrays;
+
+public class g1 {
+public static void main(String[] args){
+	int a[]={6,4,2,1,3};
+	int temp=0;
+	for(int i=0;i<a.length-1;i++){
+		if(a[i]==i){
+			System.out.println(i);
+		}
+
+}}}
