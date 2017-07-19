586aeff4402ba0a043a0bbb9873cb9bd7e6c5933
DeepakAarthi <aarthiselvamani3@gmail.com>
2017-07-19 11:22:45 +0530

    Create repeat.java

---
+++ repeat.java
@@ -0,0 +1,10 @@
+package repeat;
+public class repeat{
+public static void main(String[] args){
+int a[]={1,2,3,4,5,2};
+for(int i=0;i<a.length-1;i++){
+for(int j=0;j<a.length;j++){
+if(a[i]==a[j]){
+System.out.println(a[i]);
+}}}
+}}
