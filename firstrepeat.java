e423de332e533bdcd0c3fb7f8983b47deb90a621
DeepakAarthi <aarthiselvamani3@gmail.com>
2017-07-20 10:06:46 +0530

    Create firstrepeat.java

---
+++ firstrepeat.java
@@ -0,0 +1,17 @@
+package guvi;
+
+public class firstrepeat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a[]={2,3,4,5,2,6};
+for(int i=0;i<a.length;i++){
+	for(int j=i+1;j<a.length;j++){
+		if(a[i]==a[j]){
+			System.out.println(a[i]);
+		}
+	}
+}
+	}
+
+}
