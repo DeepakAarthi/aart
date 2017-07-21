5beaa814cc5a6c92abeebaab829330f637e7de10
DeepakAarthi <aarthiselvamani3@gmail.com>
2017-07-21 16:56:30 +0530

    Create greatestno.java

---
+++ greatestno.java
@@ -0,0 +1,13 @@
+public greatest{
+public static void main(String[] args){
+int a=10;int b=20;int c=30;
+if(a>b&&a>c){
+System.out.println(a);
+}
+if else(b>a&&b>c){
+System.out.println(b);
+}
+else{
+System.out.println(c);
+}
+}}
