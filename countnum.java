282c281bde92e146e9d0f14106b8769088e13ba6
DeepakAarthi <aarthiselvamani3@gmail.com>
2017-07-21 16:49:46 +0530

    Create countnum.java

---
+++ countnum.java
@@ -0,0 +1,11 @@
+public countnum{
+public static void main(String[] args){
+String a="ja8va93";
+int count=0;
+for(int i=0;i<a.length();i++){
+String b=a.substring(i,i+1);
+if(b=>0&&b<=9){
+count++;
+}}
+System.out.println(count);
+}}
