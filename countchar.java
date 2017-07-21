f638a191bcbfc69bafeedf2ac9641dfb97090052
DeepakAarthi <aarthiselvamani3@gmail.com>
2017-07-21 16:45:23 +0530

    Create countchar.java

---
+++ countchar.java
@@ -0,0 +1,9 @@
+public countchar{
+public static void main(String[] args){
+String a="java";
+int count=0;
+for(int i=0;i<a.length();i++){
+String b=a.substring(i,i+1);
+count++;}
+System.out.println(count);
+}}
