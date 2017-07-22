c99d2ab0a3c74afda136d22415e5037bd5a88340
DeepakAarthi <aarthiselvamani3@gmail.com>
2017-07-22 11:04:55 +0530

    Create greatestten.java

---
+++ greatestten.java
@@ -0,0 +1,10 @@
+public greatest{
+public static void main(String[] args){
+int a[]={1,2,3,4,5,6,7,8,9,0};
+int c=0,i=0;
+for( i=0;i<a.length();i++){
+if(a[i]>a[i+1]){
+c++;
+}
+System.out.println(a[i]);
+}}
